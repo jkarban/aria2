3abcafc694d51f1625e8bc262eb7f53dc391014a
jkarban <70719427+jkarban@users.noreply.github.com>
2020-09-03 17:07:31 -0400

    Add files via upload

---
+++ ocdownloader-aria2.sh
@@ -0,0 +1,4 @@
+apt install -y aria2
+apt install -y rpcbind
+apt Install -y daemon
+aria2c --daemon --enable-rpc=true
---
+++ uninstall-ocdownloader-aria2.sh
@@ -0,0 +1,3 @@
+sudo apt-get remove -y aria2
+sudo apt-get remove -y rpcbind
+sudo apt-get remove -y daemon
